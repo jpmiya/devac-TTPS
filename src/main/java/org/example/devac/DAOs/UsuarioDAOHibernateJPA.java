2085e308600ad40a27a2ebc5130583162dfbeb0b
Nicolas Beiserman <nbeisermann@gmail.com>
2025-10-22 03:46:08 -0300

    corrected bug refered to the variable name "mail".

--- src/main/java/org/example/devac/DAOs/UsuarioDAOHibernateJPA.java
+++ src/main/java/org/example/devac/DAOs/UsuarioDAOHibernateJPA.java
@@ -13,12 +13,14 @@ public class UsuarioDAOHibernateJPA extends GenericDAOHibernateJPA
 
 
     public Usuario getByMail(String mail) {
+        //get the entity Manager
         EntityManager em = EMF.getEMF().createEntityManager();
         Usuario usr;
         try {
+            //we get the user from the database
             usr = (Usuario) em.createQuery("SELECT m FROM " +
-                            this.getPersistentClass().getSimpleName() + " m WHERE m.email = :email")
-                    .setParameter("email", mail).getSingleResult();
+                            this.getPersistentClass().getSimpleName() + " m WHERE m.mail = :mail")
+                    .setParameter("mail", mail).getSingleResult();
         } catch (Exception e) {
             usr = null;
         } finally {
