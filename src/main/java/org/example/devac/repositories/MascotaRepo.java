d998b27b4e4d1077972281bfe05c2bc6b2ed1d21
Nicolas Beiserman <nbeisermann@gmail.com>
2025-11-13 21:54:35 -0300

    added findAllLost on MascotaService

--- src/main/java/org/example/devac/repositories/MascotaRepo.java
+++ src/main/java/org/example/devac/repositories/MascotaRepo.java
@@ -5,13 +5,13 @@ import org.example.devac.models.Mascota;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MascotaRepo extends  JpaRepository<Mascota,Long>{
     Optional<Mascota> findById(long id);
     Optional<Mascota> findByNombre(String nombre);
-    Optional<Mascota> findByEstado(EstadoMascota estado);
-
+    List<Mascota> findAllByEstado(EstadoMascota estado);
 }
 
--- src/main/java/org/example/devac/services/MascotaServiceImpl.java
+++ src/main/java/org/example/devac/services/MascotaServiceImpl.java
@@ -1,11 +1,13 @@
 package org.example.devac.services;
 
+import org.example.devac.models.EstadoMascota;
 import org.example.devac.models.Mascota;
 import org.example.devac.repositories.MascotaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -26,6 +28,13 @@ public class MascotaServiceImpl implements MascotaService {
 
     public void eliminar(Mascota mascota) {
         mascotaRepo.delete(mascota);
+    }
+
+    public List<Mascota> findAllLost() {
+        List<Mascota> perdidos = mascotaRepo.findAllByEstado(EstadoMascota.PERDIDO_AJENO);
+        List<Mascota> perdidosMios = mascotaRepo.findAllByEstado(EstadoMascota.PERDIDO_PROPIO);
+        perdidos.addAll(perdidosMios);
+        return perdidos;
     }
 
 
