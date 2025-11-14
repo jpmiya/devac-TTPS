3ed63dd672f5682e8d542b295d83d8fab7af82fb
Nicolas Beiserman <nbeisermann@gmail.com>
2025-11-13 22:22:18 -0300

    added controller getAllLost

--- src/main/java/org/example/devac/controllers/MascotaController.java
+++ src/main/java/org/example/devac/controllers/MascotaController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/mascota")
 public class MascotaController {
@@ -23,4 +25,10 @@ public class MascotaController {
     public ResponseEntity<Mascota> editar(@RequestBody Mascota mascota) {
         return ResponseEntity.ok(mascotaService.editar(mascota));
     }
+
+    @GetMapping("/getAllLost")
+    public ResponseEntity<List<Mascota>> getAllLost() {
+        return ResponseEntity.ok(mascotaService.getAllLost());
+    }
+
 }
--- src/main/java/org/example/devac/services/MascotaService.java
+++ src/main/java/org/example/devac/services/MascotaService.java
@@ -1,11 +1,14 @@
 package org.example.devac.services;
 
 import org.example.devac.models.Mascota;
+
+import java.util.List;
 import java.util.Map;
 
 public interface MascotaService {
     Mascota registrar(Mascota mascota);
     Mascota editar(Mascota mascota);
     void eliminar(Mascota mascota);
+    List<Mascota> getAllLost();
     //prueba
 }
