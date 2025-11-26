db67ab271023f23b9a78f366a0abcc9676c437c9
juampi <jpmiya82@gmail.com>
2025-11-26 17:38:38 -0300

    fix: if a mascota doesnt exist should return 404 not found

--- src/main/java/org/example/devac/services/MascotaEditerService.java
+++ src/main/java/org/example/devac/services/MascotaEditerService.java
@@ -1,5 +1,6 @@
 package org.example.devac.services;
 
+import jakarta.ws.rs.NotFoundException;
 import org.example.devac.exceptions.BadRequestException;
 import org.example.devac.models.Mascota;
 import org.example.devac.repositories.MascotaRepo;
@@ -17,7 +18,7 @@ public class MascotaEditerService {
 
     public Mascota edit(Long id, Mascota cambios) {
         Mascota existente = mascotaRepository.findById(id)
-                .orElseThrow(() -> new BadRequestException("Mascota no encontrada con ID: " + id));
+                .orElseThrow(() -> new NotFoundException("Mascota no encontrada con ID: " + id));
 
         String [] ignorar = PropertyUtils.getNullPropertyNames(cambios);
         BeanUtils.copyProperties(cambios, existente, ignorar);
