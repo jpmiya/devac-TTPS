dd9ef818db05448d5c45ae65be3ba08956ab5ef1
juampi <jpmiya82@gmail.com>
2025-11-26 17:27:37 -0300

    refactor: remove if statements from MascotaEditerService. Now it uses "BeanUtils" to update the existing object, along with a new "getNullPropertyNames" util to ignore the null attributes/fields.

--- src/main/java/org/example/devac/services/MascotaEditerService.java
+++ src/main/java/org/example/devac/services/MascotaEditerService.java
@@ -3,6 +3,8 @@ package org.example.devac.services;
 import org.example.devac.exceptions.BadRequestException;
 import org.example.devac.models.Mascota;
 import org.example.devac.repositories.MascotaRepo;
+import org.example.devac.utils.PropertyUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,54 +19,8 @@ public class MascotaEditerService {
         Mascota existente = mascotaRepository.findById(id)
                 .orElseThrow(() -> new BadRequestException("Mascota no encontrada con ID: " + id));
 
-        // Relación dueño
-        if (cambios.getDueno() != null) {
-            existente.setDueno(cambios.getDueno());
-        }
-
-        if (cambios.getNombre() != null) {
-            existente.setNombre(cambios.getNombre());
-        }
-
-        if (cambios.getTamaño() != null) {
-            existente.setTamaño(cambios.getTamaño());
-        }
-
-        if (cambios.getColor() != null) {
-            existente.setColor(cambios.getColor());
-        }
-
-        if (cambios.getFecha_de_perdida() != null) {
-            existente.setFecha_de_perdida(cambios.getFecha_de_perdida());
-        }
-
-        if (cambios.getEstado() != null) {
-            existente.setEstado(cambios.getEstado());
-        }
-
-        if (cambios.getFoto() != null) {
-            existente.setFoto(cambios.getFoto());
-        }
-
-        if (cambios.getCoordenadas() != null) {
-            existente.setCoordenadas(cambios.getCoordenadas());
-        }
-
-        if (cambios.getDescripcion() != null) {
-            existente.setDescripcion(cambios.getDescripcion());
-        }
-
-        if (cambios.getAvistamientos() != null) {
-            existente.setAvistamientos(cambios.getAvistamientos());
-        }
-
-        if (cambios.getTipo() != null) {
-            existente.setTipo(cambios.getTipo());
-        }
-
-        if (cambios.getRaza() != null) {
-            existente.setRaza(cambios.getRaza());
-        }
+        String [] ignorar = PropertyUtils.getNullPropertyNames(cambios);
+        BeanUtils.copyProperties(cambios, existente, ignorar);
 
         return mascotaRepository.save(existente);
     }
